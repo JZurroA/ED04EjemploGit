1aa96f0597f70cfcbbbadac20b99e7bdf1186bb2
Jabier Zurro Aduriz <jzurro@birt.eus>
2024-05-01 17:35:37 +0200

    Version 1. Commit con cambios basicos

--- src/main/java/ed/ud04ejemplogit/EjemploGit.java
+++ src/main/java/ed/ud04ejemplogit/EjemploGit.java
@@ -1,13 +1,12 @@
-
 package ed.ud04ejemplogit;
 
 /**
- *
+ * Descripcion de la clase
  * @author Jabier Zurro Aduriz
  */
 public class EjemploGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Ejemplo de Git");
     }
 }
