dca5b45d5d31afdb8310a00107e347115a97ed55
Jabier Zurro Aduriz <jzurro@birt.eus>
2024-05-01 17:33:15 +0200

    Version 0. Primer commit del proyecto

---
+++ src/main/java/ed/ud04ejemplogit/EjemploGit.java
@@ -0,0 +1,13 @@
+
+package ed.ud04ejemplogit;
+
+/**
+ *
+ * @author Jabier Zurro Aduriz
+ */
+public class EjemploGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
